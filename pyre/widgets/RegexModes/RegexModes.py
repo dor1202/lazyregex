f06c6b7ec72b65620042cfee89d667796637f233
dor-schreiber <dor.schreiber@pipl.com>
2024-12-26 20:05:29 +0200

    Improve footer

--- pyre/widgets/RegexModes/RegexModes.py
+++ pyre/widgets/RegexModes/RegexModes.py
@@ -1,11 +1,16 @@
 from textual.widget import Widget
 from textual.app import ComposeResult
-from textual.widgets import RadioButton, RadioSet
+from textual.widgets import RadioButton, RadioSet, Label
+
+from ...GlobalState import GlobalState
 
 
 class RegexModes(Widget):
     def compose(self) -> ComposeResult:
         with RadioSet():
-            yield RadioButton("match")
+            yield RadioButton("match", value=True)
             yield RadioButton("substitution")
             yield RadioButton("search")
+
+    def on_radio_set_changed(self, event: RadioSet.Changed) -> None:
+        GlobalState().regex_method = event.index
